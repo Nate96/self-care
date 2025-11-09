8cf9af89d4782b4aba2208f16b922b6a9eddd7cc
Nate96 <braukhoff96@gmail.com>
2025-11-08 19:01:28 -0600

    added mui's table and graph

--- web-ui/src/containers/Stats.tsx
+++ web-ui/src/containers/Stats.tsx
@@ -1,12 +1,18 @@
 import { useState, useEffect } from "react";
-import Table from 'react-bootstrap/Table';
 import { Link } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { LineChart } from '@mui/x-charts/LineChart';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 import { BasicCalc } from "../lib/types";
-import "./../css/Stats.css";
 import FormApi from "../Services/FormApi";
-import { LineChart } from '@mui/x-charts/LineChart';
+
+import "./../css/Stats.css";
 
 const Stats = () => {
   const [basicAnalyse, setBasicAnalyse] = useState<BasicCalc[]>()
@@ -19,8 +25,6 @@ const Stats = () => {
   useEffect(() => {
     getBasicCalc()
   }, [])
-
-  console.log(basicAnalyse?.map((x) => x.create_dt))
 
   return (
     <div className="Stats">
@@ -56,46 +60,49 @@ const Stats = () => {
           height={400}
         />
       </div>
-      <div className="card-container">
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Stars</th>
-              <th>Pysical</th>
-              <th>Emotional</th>
-              <th>Social</th>
-              <th>Spirit</th>
-              <th>Professional</th>
-              <th>Total</th>
-              <th>Average</th>
-            </tr>
-          </thead>
-          <tbody>
-            {basicAnalyse?.map((row, index, arr) => {
-              return (
-                <tr key={index}>
-                  <td>
-                    <Link
-                      to="/view-assessment" 
-                      state={{details: row.assessment_id}}
-                      >
-                      {new Date(row.create_dt).toDateString()}
-                    </Link>
-                    </td>
-                  <td align="center">{row.total_stars}</td>
-                  <td align="center">{row.physical}</td>
-                  <td align="center">{row.emotional}</td>
-                  <td align="center">{row.social}</td>
-                  <td align="center">{row.spirit}</td>
-                  <td align="center">{row.professional}</td>
-                  <td align="center">{row.total}</td>
-                  <td align="center">{row.average_rank}</td>
-                </tr>
-              )
-            })}
-          </tbody>
+      <div className="section">
+        <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Date</TableCell>
+              <TableCell align="right">Stars</TableCell>
+              <TableCell align="right">Physical</TableCell>
+              <TableCell align="right">Emotional</TableCell>
+              <TableCell align="right">Socail</TableCell>
+              <TableCell align="right">Spirit</TableCell>
+              <TableCell align="right">Professional</TableCell>
+              <TableCell align="right">Average</TableCell>
+              <TableCell align="right">Total</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {basicAnalyse?.map((row, index, arr) => (
+              <TableRow
+                key={index}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  <Link
+                    to="/view-assessment" 
+                    state={{details: row.assessment_id}}
+                    >
+                    {new Date(row.create_dt).toDateString()}
+                  </Link>
+                </TableCell>
+                <TableCell align="right">{row.total_stars}</TableCell>
+                <TableCell align="right">{row.physical}</TableCell>
+                <TableCell align="right">{row.emotional}</TableCell>
+                <TableCell align="right">{row.social}</TableCell>
+                <TableCell align="right">{row.spirit}</TableCell>
+                <TableCell align="right">{row.professional}</TableCell>
+                <TableCell align="right">{row.average_rank.toFixed(2)}</TableCell>
+                <TableCell align="right">{row.total}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
         </Table>
+      </TableContainer>
         </div>
     </div>
   );
